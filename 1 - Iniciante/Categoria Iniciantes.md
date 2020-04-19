47b283d7cade9ed891f98e8b9df803fc0c01bcfd
paula-cristina <62576219+paula-cristina@users.noreply.github.com>
2020-04-19 13:58:18 -0300

    ok

---
+++ 1 - Iniciante/Categoria Iniciantes.md
@@ -0,0 +1,7 @@
+# URI ONLINE JUDGE - PROBLEMS & CONTESTS 
+> Site: https://www.urionlinejudge.com.br/judge/en/login
+
+### CATEGORIAS
+
+##### 1 - INICIANTE
+> Problemas básicos para quem está iniciando na programação...
---
+++ 1 - Iniciante/PYTHON/1001 - Extremamente Básico.py
@@ -0,0 +1,10 @@
+''' 
+    1001 - Extremamente Básico
+    https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+    Autora: Paula Cristina Martins
+    E-mail: paulacristina@unifeg.edu.br 
+'''
+
+a = int(input(""))
+b = int(input(""))
+print("X = {}". format(a+b))
